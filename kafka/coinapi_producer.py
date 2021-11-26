6fcd75cee373b191c274b570941f2a700363808c
bgremmen <bas@luton.nl>
2021-11-26 14:14:53 +0100

    joe

---
+++ kafka/coinapi_producer.py
@@ -0,0 +1,43 @@
+import websocket
+import json
+from kafka import KafkaProducer
+api_key = 'AC857EEF-D841-4FF7-BE8E-61121010D85A' #your api key
+
+
+class CoinAPIv1_subscribe(object):
+    def __init__(self, apikey):
+        self.type = "hello"
+        self.apikey = apikey
+        self.heartbeat = True
+        self.subscribe_data_type = ["trade"]
+        self.taker_side = ["BUY"]
+
+
+def on_message(ws, message):
+    if json.loads(message)['type'] == 'trade':
+        producer.send('trade', bytes(message, encoding='utf-8'))
+        print("Sending " + message)
+        producer.flush(timeout=60)
+
+
+def on_error(ws, message):
+    print(message)
+
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+
+def on_open(ws):
+    sub = CoinAPIv1_subscribe(api_key)
+    ws.send(json.dumps(sub.__dict__))
+
+
+if __name__ == "__main__":
+    producer = KafkaProducer(bootstrap_servers='34.69.175.54:9092')  # VM's external IP
+    ws = websocket.WebSocketApp("ws://ws.coinapi.io/v1/",
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.run_forever()
