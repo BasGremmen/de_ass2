c35ccdaa81ec5f3a63fdc1db4343b04effad4274
bgremmen <bas@luton.nl>
2021-12-01 13:25:34 +0100

    Go

--- deployment/dashboard/app.py
+++ deployment/dashboard/app.py
@@ -16,11 +16,12 @@ table_ref = client.dataset("ass2").table("trades")  # use the correct dataset na
 def visualize(broker):
     # Perform a query.
     QUERY = (
-        'SELECT * FROM `glass-sylph-325109.ass2.trades` WHERE broker = "' + broker + '" ORDER BY time_frame.end DESC LIMIT 100')  # use the correct project id, etc.
+        'SELECT count, time_frame.end FROM `glass-sylph-325109.ass2.trades` WHERE broker = "' + broker + '" ORDER BY time_frame.end DESC LIMIT 100')  # use the correct project id, etc.
     query_job = client.query(QUERY)  # API request
     rows = query_job.result()  # Waits for query to finish
     # Return an html file with the data in it
-    return render_template('dashboard.html', historical=[list(row) for row in rows])
+    historical = [{'x':row[1], 'y':row[0]} for row in rows]
+    return render_template('dashboard.html', historical=historical)
 
 
 app.run(host='0.0.0.0', port=5000)
